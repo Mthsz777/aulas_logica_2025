df0d555c013a5c523672ca44ffa18a8b69429001
Matheus Antonio <matheus.a.santos38@aluno.senai.br>
2025-04-22 10:08:31 -0300

    aula 18

---
+++ Exercicios/Exe09/Exe01.js
@@ -0,0 +1,6 @@
+function FormatarData(dia, mes, ano) {
+    let data = `${dia}/${mes}/${ano}`
+    return(data)
+}
+console.log(FormatarData(25, 12, 1987));
+
---
+++ Exercicios/Exe09/Exe02.js
@@ -0,0 +1,9 @@
+
+function CoresPrimarias() {
+   let lista = ["Vermelho", "Azul", "Amarelo"];
+    
+    for (let i = 0; i < lista.length; i++) {
+        console.log(lista[i]);
+    }
+}
+CoresPrimarias()
